ec4b299beafe53ec8edc8f92d8d345e3db9a5db7
YSemets <yulichka11001@gmail.com>
2023-09-08 14:30:33 +0100

    added fixes

--- hw16.js
+++ hw16.js
@@ -1,66 +1,85 @@
 
 
 /*========================================*/
+
 const strReverse = () => {
-    let str = 'hello';
-    let reverse = '';
-    for (let i = str.length - 1; i >= 0; i--){
-      reverse += str[i];
-    }
-    console.log (reverse);
-  
+  let str = 'hello';
+  let reverse = '';
+  for (let i = str.length - 1; i >= 0; i--){
+    reverse += str[i];
   }
-  strReverse();
-  
-  /*========================================*/
-  const countWords = (str) => {
-    let arr = str.split(/\s+/).length;
-  
-    console.log (arr);
-  };
-  
-  countWords("Hi my name is Taras");
-  // 5
-  
-  
-  /*========================================*/
-  const clearWhiteSpace = (str) => {
-    let arr = str.replace(/ /g, "");
-  
-    console.log (arr);
-  };
-  
-  const result = clearWhiteSpace("  1   2   ");
-  // 12
-  
-  
-  /*========================================*/
-  const cutStr = (str, maxLength) => {
-    if (str.length > maxLength){
-      console.log(str.slice(0, maxLength) + '...');
+  console.log (reverse);
+
+}
+strReverse();
+
+
+
+
+/*========================================*/
+
+
+const countWords = (str) => {
+  let arr = str.split(/\s+/).length;
+
+  console.log (arr);
+};
+
+countWords("Hi my name is Taras");
+// 5
+
+
+/*========================================*/
+
+
+const clearWhiteSpace = (str) => {
+  let arr = str.replace(/ /g, "");
+
+  console.log (arr);
+};
+
+const result = clearWhiteSpace("  1   2   ");
+// 12
+
+
+
+
+/*========================================*/
+
+
+const cutStr = (str, maxLength) => {
+  if (str.length > maxLength){
+    console.log(str.slice(0, maxLength) + '...');
+  } else {
+    console.log (str);
+  }
+};
+
+cutStr("Hello", 5);
+// "Hello"
+cutStr("Hello there", 5);
+// "Hello..."
+
+
+
+/*========================================*/
+
+const palindrome = (str) => {
+  const temp = str.length;
+  const calc = Math.floor(temp / 2)
+
+  for (let i = 0; i < calc ; i++) {
+    if (str.charAt(i) !== str.charAt(temp - 1 - i)) {
+      console.log('false');
+    }else{
+      console.log('true');
     }
-  };
+  }
   
-  cutStr("Hello", 5);
-  // "Hello"
-  cutStr("Hello there", 5);
-  // "Hello..."
-  
-  
-  
-  /*========================================*/
-  const palindrome = (str) => {
-    const temp = str.split('').reverse().join('');
-    if (str === temp){
-      console.log ('true');
-    } else 
-      console.log ('false');
-    
-      
-  };
-  
-  palindrome("tenet");
-  // true
-  palindrome("js");
-  // false
-  
+};
+
+palindrome("tenet");
+// true
+//palindrome("js");
+// false
+
